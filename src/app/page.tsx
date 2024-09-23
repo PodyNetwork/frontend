fbc4e90d94927f46984ed30e49ad120100abcee7
olagunjuea1 <olagunjuea1@gmail.com>
2024-09-23 16:43:31 +0100

    🎉 fresh start

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="relative float-left w-full h-full overflow-hidden" aria-label="Homepage">
+      <section className="w-full relative min-h-screen" aria-labelledby="hero">
+        <div>
+          
+        </div>
+      </section>
+    </div>
+  );
+}
