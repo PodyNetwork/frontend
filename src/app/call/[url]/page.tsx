3a8abe87c597403bd171e48ea5c8da817c179b33
Oyeniyi Abiola Peace <officialbilas@gmail.com>
2024-10-04 09:05:00 +0100

    fixed loop

--- src/app/call/[url]/page.tsx
+++ src/app/call/[url]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import MeetLayout from '@/components/meet/meetLayout'
 import { useParams } from 'next/navigation';
 import useGetCallByURL from '@/hooks/call/useGetCallByURL';
@@ -11,11 +11,14 @@ const Meet = () => {
   
   const { createCallToken, accessToken } = useCreateCallToken();
 
+  const [isTokenFetched, setIsTokenFetched] = useState<boolean>(false)
+
   useEffect(() => {
-    if (call) { 
+    if (call && !isTokenFetched) { 
       createCallToken.mutate({ callId: call._id });
+      setIsTokenFetched(true)
     }
-  }, [call, createCallToken]);
+  }, [call, isTokenFetched, createCallToken]);
 
   useEffect(() => {
     if (accessToken) {
