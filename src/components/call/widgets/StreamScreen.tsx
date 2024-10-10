4faf46285d45c28621b37f4053cd2e284a5d0151
olagunjuea1 <olagunjuea1@gmail.com>
2024-10-10 19:36:08 +0100

    🚀 update stream page mobile

--- src/components/call/widgets/StreamScreen.tsx
+++ src/components/call/widgets/StreamScreen.tsx
@@ -11,8 +11,7 @@ import useGetCallByURL from "@/hooks/call/useGetCallByURL";
 import { useParams } from "next/navigation";
 import { EnhancedFocusLayout } from "../livekitcustom/FocusLayoutTile";
 import { useState } from "react";
-import { shareOnMobile } from "react-mobile-share";
-import useProfile from "@/hooks/user/useProfile";
+import StreamShare from "./StreamShare";
 
 const MyVideoConference = () => {
   const tracks = useTracks(
@@ -47,7 +46,6 @@ const StreamScreen = () => {
   const { url } = useParams();
   const { call } = useGetCallByURL(url as string);
   const participants = useParticipants();
-  const { profile, isLoading, isError } = useProfile();
 
   return (
     <>
@@ -76,33 +74,7 @@ const StreamScreen = () => {
               <path d="M103.85-215.38v-65.85q0-27.85 14.42-47.89 14.42-20.03 38.76-32.02 52.05-24.78 103.35-39.51 51.31-14.73 123.47-14.73 72.15 0 123.46 14.73 51.31 14.73 103.35 39.51 24.34 11.99 38.76 32.02 14.43 20.04 14.43 47.89v65.85h-560Zm640 0v-67.7q0-34.77-14.08-65.64-14.07-30.87-39.92-52.97 29.46 6 56.77 16.65 27.3 10.66 54 23.96 26 13.08 40.77 33.47 14.76 20.4 14.76 44.53v67.7h-112.3Zm-360-289.24q-49.5 0-84.75-35.25t-35.25-84.75q0-49.5 35.25-84.75t84.75-35.25q49.5 0 84.75 35.25t35.25 84.75q0 49.5-35.25 84.75t-84.75 35.25Zm290.77-120q0 49.5-35.25 84.75t-84.75 35.25q-2.54 0-6.47-.57-3.92-.58-6.46-1.27 20.33-24.9 31.24-55.24 10.92-30.34 10.92-63.01t-11.43-62.44q-11.42-29.77-30.73-55.62 3.23-1.15 6.46-1.5 3.23-.35 6.47-.35 49.5 0 84.75 35.25t35.25 84.75ZM143.85-255.38h480v-25.85q0-14.08-7.04-24.62-7.04-10.53-25.27-20.15-44.77-23.92-94.39-36.65-49.61-12.73-113.3-12.73-63.7 0-113.31 12.73-49.62 12.73-94.39 36.65-18.23 9.62-25.27 20.15-7.03 10.54-7.03 24.62v25.85Zm240-289.24q33 0 56.5-23.5t23.5-56.5q0-33-23.5-56.5t-56.5-23.5q-33 0-56.5 23.5t-23.5 56.5q0 33 23.5 56.5t56.5 23.5Zm0 289.24Zm0-369.24Z" />
             </svg>
           </p>
-          <div>
-            {!isLoading ||
-              (!isError && (
-                <button
-                  onClick={() =>
-                    shareOnMobile({
-                      text: `"Hey ${profile?.username} invited you to their call"`,
-                      url: `"https://pody.network/${call?.url}"`,
-                      title: "Pody Meeting",
-                      // images: [imgBase64],
-                    })
-                  }
-                >
-                  <p className="w-7 h-7 flex items-center justify-center bg-slate-800 rounded-full">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-5 h-5 text-slate-600"
-                      viewBox="0 -960 960 960"
-                      style={{ msFilter: "" }}
-                      fill="currentColor"
-                    >
-                      <path d="M730-420v-120H610v-40h120v-120h40v120h120v40H770v120h-40Zm-370-84.62q-49.5 0-84.75-35.25T240-624.62q0-49.5 35.25-84.75T360-744.62q49.5 0 84.75 35.25T480-624.62q0 49.5-35.25 84.75T360-504.62ZM80-215.38v-65.85q0-24.77 14.42-46.35 14.43-21.57 38.81-33.5 56.62-27.15 113.31-40.73 56.69-13.57 113.46-13.57 56.77 0 113.46 13.57 56.69 13.58 113.31 40.73 24.38 11.93 38.81 33.5Q640-306 640-281.23v65.85H80Zm40-40h480v-25.85q0-13.31-8.58-25-8.57-11.69-23.73-19.77-49.38-23.92-101.83-36.65-52.45-12.73-105.86-12.73t-105.86 12.73Q201.69-349.92 152.31-326q-15.16 8.08-23.73 19.77-8.58 11.69-8.58 25v25.85Zm240-289.24q33 0 56.5-23.5t23.5-56.5q0-33-23.5-56.5t-56.5-23.5q-33 0-56.5 23.5t-23.5 56.5q0 33 23.5 56.5t56.5 23.5Zm0-80Zm0 369.24Z" />
-                    </svg>
-                  </p>
-                </button>
-              ))}
-          </div>
+          <StreamShare />
         </div>
       </div>
       <div className="relative flex flex-col gap-y-1">
