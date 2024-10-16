f1207701be0856ce809520212a0d5d5793055583
olagunjuea1 <olagunjuea1@gmail.com>
2024-10-16 17:37:35 +0100

    ✨ update Ui

--- src/components/Avatar/AvatarParticipant.tsx
+++ src/components/Avatar/AvatarParticipant.tsx
@@ -2,6 +2,6 @@ import Avatar from "boring-avatars";
 
 export const AvatarParticipant = ({name} : {name: string}) => (
   <>
-    <Avatar name={name} className="w-full h-full" variant="beam" colors={["#fb6900", "#f63700", "#004853", "#007e80", "#00b9bd"]}/>
+    <Avatar name={name} className="w-full h-full" variant="beam" colors={["#07070A", "#f63700", "#004853", "#007e80", "#00b9bd"]}/>
   </>
 );
--- src/components/call/widgets/Audio/AudioAnalyzerPody.tsx
+++ src/components/call/widgets/Audio/AudioAnalyzerPody.tsx
@@ -7,16 +7,14 @@ const AudioAnalyzer = ({ track }: { track: LocalAudioTrack | RemoteAudioTrack })
   const { bars } = useAudioWaveform(track, { barCount: 4, volMultiplier: 4 });
 
   return (
-    <div className="flex justify-center items-center h-[20px]">
+    <div className="flex justify-center items-center h-[13px] md:h-[20px]">
       {bars.map((bar, index) => (
         <div
           key={index}
           className="bg-slate-700 dark:bg-slate-400 rounded-lg mx-px transition-all duration-100 ease-in-out"
           style={{
-            height: `${Math.min(bar * 30, 15)}px`, // Limit the height to a maximum of 28px
-            width: '3px',                      
-            maxHeight: '28px',                  
-            maxWidth: '3px',                  
+            height: `${Math.min(bar * 100, 80)}%`,  // Limit the height to a maximum of 28px
+            width: '3px'                  
           }}
         />
       ))}
