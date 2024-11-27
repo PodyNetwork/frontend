60a9c065805c43cb31686e880de5854313b8ad42
olagunjuea1 <olagunjuea1@gmail.com>
2024-11-27 21:10:53 +0100

    🐛 update v2 Auth page

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -1,15 +1,41 @@
 "use client";
 import AuthIntro from "@/components/Auth/AuthIntro";
 import React, { ReactNode } from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
-const auth = ({children}: {children: ReactNode}) => {
+const auth = ({ children }: { children: ReactNode }) => {
   return (
-    <section className="bg-black/80 w-full">
+    <section className="w-full">
+      <div className="absolute inset-0 w-full h-full overflow-hidden pointer-events-none">
+        <motion.div
+          className="absolute -bottom-28 w-full flex justify-center blur-2xl"
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{
+            opacity: [0.9, 1, 0.9],
+            scale: [1, 1.2, 1],
+          }}
+          transition={{
+            duration: 10,
+            repeat: Infinity,
+            repeatType: "mirror",
+            delay: 0.6,
+          }}
+        >
+          <Image
+            src="/abstract/hero17.png"
+            width={500}
+            height={500}
+            className="object-contain w-[22rem] md:w-[33rem] sm:w-[26rem]"
+            alt="Hero abstract background 3"
+          />
+        </motion.div>
+      </div>
       <div className="h-screen flex flex-col items-center justify-center">
-        <div className="w-full flex flex-col md:flex-row bg-white __shadow_pody mx-auto h-full">
+        <div className="w-full flex flex-col md:flex-row mx-auto h-full">
           <AuthIntro />
           <div className="md:w-1/2 lg:w-[35rem] flex flex-col justify-center items-center h-full px-5">
-          {children}
+            {children}
           </div>
         </div>
       </div>
