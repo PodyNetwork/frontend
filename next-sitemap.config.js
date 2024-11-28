2bc75ab1ce9f47299769538fa7db51427f5c0560
olagunjuea1 <olagunjuea1@gmail.com>
2024-11-28 08:58:20 +0100

    🚀 Patched sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteUrl: 'https://pody.network',
     generateRobotsTxt: true, 
-    exclude: ['/dashboard*', '/callend', '/v2', '/test', '/callv2', 'call']
+    exclude: ['/dashboard*', '/callend', '/v2', '/test', '/callv2', 'call', 'discord']
 }
   
