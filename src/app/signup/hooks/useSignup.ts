ad5097e59c44c8b6d18b71c27e6decfd06d33cd4
olagunjuea1 <olagunjuea1@gmail.com>
2024-12-18 15:57:23 +0100

    🐛 updated referral

--- src/app/signup/hooks/useSignup.ts
+++ src/app/signup/hooks/useSignup.ts
@@ -1,38 +1,21 @@
-"use client";
-
+"use client"
 import { useMutation } from '@tanstack/react-query';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import type { Response, ResponseError } from '@/types/globals';
-import axios from "@/network/axios";
+import axios from "@/network/axios"
 import { AxiosError, isAxiosError } from 'axios';
 import useErrorMessage from '../../../hooks/useErrorMessage';
 import handleCreatePassport from '../utils/handleCreatePassport';
 import { useRouter } from 'next/navigation';
 
-type SignupPayload = {
-  username: string;
-  referralCode?: string | null;
-};
-
 const useSignup = () => {
   const { errorMessage, setErrorMessage, clearErrorMessage } = useErrorMessage();
   const router = useRouter();
-  const [referralCode, setReferralCode] = useState<string | null>(null);
-
-  useEffect(() => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const refParam = queryParams.get("ref");
-    setReferralCode(refParam);
+  const signupHandler = useCallback(async ( { username }: { username: string }): Promise<Response> => {
+    const credentials = await handleCreatePassport({username});
+    const response = await axios.post<Response>('/auth/signup', credentials);
+    return response.data;
   }, []);
-
-  const signupHandler = useCallback(
-    async ({ username }: SignupPayload): Promise<Response> => {
-      const credentials = await handleCreatePassport({ username, referralCode });
-      const response = await axios.post<Response>('/auth/signup', credentials);
-      return response.data;
-    },
-    [referralCode]
-  );
 
   const signup = useMutation({
     mutationFn: signupHandler,
@@ -44,14 +27,13 @@ const useSignup = () => {
       if (isAxiosError(error)) {
         const errorData = error?.response?.data as ResponseError;
         setErrorMessage(errorData.message, 'error');
-        return;
+        return; 
       }
       setErrorMessage(error.message, 'error');
     },
   });
 
-  return { signup, errorMessage, referralCode };
-};
+  return { signup, errorMessage };
+}
 
-export default useSignup;
-
+export default useSignup
