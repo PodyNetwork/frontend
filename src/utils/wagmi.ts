a3959fd24995483fb7285124a09b60bf5ce61762
Oyeniyi Abiola Peace <officialbilas@gmail.com>
2025-01-24 16:48:36 +0100

    fixed network

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -1,9 +1,11 @@
 import { EDUChainMainnet, OpenCampusCodexSepolia } from "@/utils/chain";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 
+const network = Boolean(process.env.NEXT_PUBLIC_ENABLE_TESTNETS) ? EDUChainMainnet : OpenCampusCodexSepolia;
+
 export const config = getDefaultConfig({
   appName: "Pody",
   projectId: "d4a6f4edae5c0b3b2352b1e169c92123",
-  chains: [OpenCampusCodexSepolia, EDUChainMainnet],
+  chains: [network],
   ssr: true,
 });
