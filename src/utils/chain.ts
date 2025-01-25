f744f8f1426c7e791d3f488f428a156033fdf976
Oyeniyi Abiola Peace <officialbilas@gmail.com>
2025-01-25 23:10:45 +0100

    final fix for environment variable

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -1,3 +1,3 @@
 import { Chain, defineChain } from "viem";
 
-export const DefaultChain = defineChain(JSON.parse(process.env.CHAIN_CONFIGURATION as string) as Chain);
+export const DefaultChain = defineChain(JSON.parse(process.env.NEXT_PUBLIC_ENABLE_TESTNETS as string) as Chain);
