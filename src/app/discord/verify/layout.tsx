77b17adf8cda2ba70a5780e5ad3315fe18b0f1d1
Oyeniyi Abiola Peace <officialbilas@gmail.com>
2025-01-31 20:59:53 +0100

    add auth to discord

---
+++ src/app/discord/verify/layout.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import AuthMiddleware from "@/middlewares/AuthMiddleware";
+
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <AuthMiddleware>{children}</AuthMiddleware>
+}
+
+export default Layout
+
