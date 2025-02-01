c23260a459227d0a4ad795b711a4470e0e18e95e
olagunjuea1 <olagunjuea1@gmail.com>
2025-02-01 04:34:06 +0100

    fix username truncate

--- src/components/dashboard/widgets/Leaderboard/TopThree.tsx
+++ src/components/dashboard/widgets/Leaderboard/TopThree.tsx
@@ -35,10 +35,10 @@ export const TopThree = ({ data }: { data: LeaderboardEntry[] }) => {
                 </span>
               </div>
             </div>
-            <h2 className="font-medium text-sm mt-2 text-slate-800 truncate">
+            <h2 className="font-medium text-sm mt-2 text-slate-800 truncate max-w-24">
               {item?.username}
             </h2>
-            <p className="text-sm text-pody-primary font-semibold truncate max-w-24">
+            <p className="text-sm text-pody-primary font-semibold truncate">
                {/* {approx(Number(formatUnits(BigInt(item?.totalPoints ?? 0), 18)), {decimal: false})} */}
                {formatNum(Number(formatUnits(BigInt(item?.totalPoints ?? 0), 18)))}
             </p>
