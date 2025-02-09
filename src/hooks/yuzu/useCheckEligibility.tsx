0bfaadeac084cfb8619de8099d1c6e3b5843a215
Oyeniyi Abiola Peace <officialbilas@gmail.com>
2025-02-09 23:51:31 +0100

    fixed yuzu issues

--- src/hooks/yuzu/useCheckEligibility.tsx
+++ src/hooks/yuzu/useCheckEligibility.tsx
@@ -3,30 +3,44 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "@/network/axios";
 import { BaseResponse } from "@/types/globals";
+import queryClient from "@/utils/queryClient";
+import { useEffect, useState } from "react";
+import { set } from "date-fns";
 
 interface EligibilityResponse extends BaseResponse {
   data: {
     eligible: boolean;
-    reason?: string;
   };
 }
 
 const useCheckEligibility = () => {
-  const { data, isLoading, isError, refetch } = useQuery<EligibilityResponse, Error>({
+
+  const [retries, setRetries] = useState(0);
+
+  const fetchEligibility =  async () => {
+    const response = await axios.get<EligibilityResponse>("/yuzu/eligibility");
+    return response.data;
+  }
+  
+  const { data, isLoading, isError, error, refetch } = useQuery<EligibilityResponse, Error>({
     queryKey: ["eligibility"],
-    queryFn: async () => {
-      const response = await axios.get<EligibilityResponse>("/yuzu/eligibility");
-      return response.data;
-    },
-    retry: false, 
+    queryFn: fetchEligibility,
+    retry: 0,
     staleTime: 0,
-    refetchOnMount: true, 
+    refetchOnMount: true,
     refetchOnWindowFocus: true,
   });
-  
-  const isEligible = data?.data?.eligible === true;
+
+  useEffect(() => {
+    if (error && retries < 1) {
+      queryClient.invalidateQueries();
+      queryClient.removeQueries();
+      setRetries((prev) => prev + 1);
+    }
+  }, [error, retries])
+
   return {
-    isEligible: isEligible ?? false,
+    isEligible: data?.data.eligible,
     isLoading,
     isError,
     refetch,
