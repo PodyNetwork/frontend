b06568a528bc4f23df745fb6597e1bfd793cca69
olagunjuea1 <olagunjuea1@gmail.com>
2025-02-10 22:27:33 +0100

    udated middleward

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/yuzu3/eligibility')) {
+  if (request.nextUrl.pathname.startsWith('/yuzu/eligibility')) {
     if (!request.headers.get('host')?.includes('testnet')) {
       return NextResponse.redirect('https://pody.network/404')
     }
