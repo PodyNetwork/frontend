924b3c956b93c0655facbe5ebf08be38d4019a27
olagunjuea1 <olagunjuea1@gmail.com>
2025-03-10 17:57:42 +0100

    🔥 updated Yuzu typo

--- src/app/yuzu/eligibility/page.tsx
+++ src/app/yuzu/eligibility/page.tsx
@@ -228,8 +228,8 @@ const Page = () => {
                     ) : (
                       <>
                         {isEligible
-                          ? "You are eligible for the Pody Yuzu Season 1 Yuzu."
-                          : "You are not eligible for the Pody Yuzu Season 1 Yuzu. More season coming soon"}
+                          ? "You are eligible for the Pody Season 1 Yuzu."
+                          : "You are not eligible for the Pody Season 1 Yuzu. More season coming soon"}
                       </>
                     )}
                   </div>
